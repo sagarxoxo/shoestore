822f7307a2adf10cd7206c87eb321acf25ef1c11
sagarxoxo <mss82605@gmail.com>
2022-12-06 22:03:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import SingleProduct from './pages/SingleProduct/SingleProduct';
+import Cart from './pages/Cart/Cart';
+import Contact from './pages/Contact/Contact';
 
 function App() {
 
-  const options = {
-    method: 'GET',
-    url: 'https://shoes-collections.p.rapidapi.com/shoes',
-    headers: {
-      'X-RapidAPI-Key': 'afd037ad37mshcebc8e0f06321dep155c19jsn88bee3dcc6a7',
-      'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
-    }
-  };
-
-  useEffect(() => {
-  
-    fetch("https://shoes-collections.p.rapidapi.com/shoes", {
-      method: "GET",
-      withCredentials: true,
-      headers: {
-        'X-RapidAPI-Key': 'afd037ad37mshcebc8e0f06321dep155c19jsn88bee3dcc6a7',
-        'X-RapidAPI-Host': 'shoes-collections.p.rapidapi.com'
-      }
-    })
-      .then(resp => resp.json())
-      .then(function(data) {
-        console.log(data);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-      
-  }, [])
-  
-
-  console.log(options)
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />}/>
+          <Route path="/cart" exact element={<Cart />}/>
+          <Route path="/contact" exact element={<Contact />}/>
+          <Route path="/product/:id" exact element={<Product />}/>
+          <Route path="/singleproduct/:id" exact element={<SingleProduct />}/>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/constants/CONSTANT_DATA.js
@@ -0,0 +1,18 @@
+export const HeroSection = [
+    {
+        shoeType: "Men's Shoe",
+        headerTitle: "New Jordan 6 Rings",
+        desc: "The Jordan 6 Rings Men's Shoe celebrates a legendary career with classic design lines and detailing for streamlined hoops style.",
+    },
+    {
+        subTitle: "New from nike sportswear",
+        headerTitle: "REACT PRESTO",
+        desc: "With React foam for the most comfortable Presto ever.",
+    },
+    {
+        subTitle: "Create your design",
+        headerTitle: "NIKE REACT PRESTO BY YOU",
+        desc: "Take advantage of brand new, proprietary cushioning technology with a fresh pair of Nike React shoes.",
+    }
+]
+export const TrendingNowSec2 = ["Summer Must-Haves: Air Max Dia", "Air Jordan 11 Retro Low LE"]
